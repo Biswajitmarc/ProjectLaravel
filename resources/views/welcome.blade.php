7f92e6d9a7b5829abbe32a2edaeda4a5e1225f15
Biswajitmarc <biswajit.mondal80@gmail.com>
2020-07-05 08:33:22 +0530

    adding files

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends ('layouts/app')
+
+@section('title','Project1')
+
+@section('content')
+<section class="banner">
+<div class="container-fluid">
+    <div class="row">
+        <div class='col'>
+            <img src="{{url('/images/universe-1566159_1920.jpg')}}" alt="banner-image"/>
+        </div>  
+    </div>
+    <div>
+</div>
+</section> 
+
+@endsection
+
